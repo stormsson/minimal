86939be65eef16c172b140292c2373b816bef288
Camillo Camarda <camillo.camarda@akqa.com>
2023-04-18 18:24:33 +0200

    minimal_reproduction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Projects.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
