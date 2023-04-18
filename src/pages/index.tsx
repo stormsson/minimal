36224200d854c4e71ac430cd5436af99682f3174
Camillo Camarda <camillo.camarda@akqa.com>
2023-04-18 18:25:11 +0200

    minimal_reproduction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Projects({projects}: any) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>        
+      <main>        
         This is the index
       </main>
     </>
